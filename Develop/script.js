2e96f72e35c8933e4e305752c0f299666e6d1793
devin <devin@devins-MacBook-Pro.local>
2022-07-10 14:00:37 -0400

    pushing final edit of JS file and README

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,179 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var number = "0123456789"
+var special = "~`!@#$%^&*()-_=+[]{}|/;:'<>.,?"
+// var passwordLength; 
+// var lowercaseOpt; 
+// var uppercaseOpt; 
+// var numberOpt; 
+// var specialOpt; 
+// var password; (password output)
+
+// Function used to determine length of password
+function determineLength(){
+  passwordLength = prompt("Decide password Length - Choose between 8-128 characters:");
+
+  if (passwordLength<8){
+    alert("Password length must be a number between 8-128 characters.");
+    determineLength();
+  } else if (passwordLength>128){
+    alert("Password length must be a number between 8-128 characters.");
+    determineLength();
+  } else if (isNaN(passwordLength)){
+    alert("Password length must be a number between 8-128 characters.");
+    determineLength();
+  } else {
+    alert("The following prompts will ask you to provide further specifications in order to generate your password. Press 'OK' to continue.");
+  }
+  return passwordLength;
+} 
+
+// Function used to determine inclusion of lowercase letters in password
+function chooseLowercase(){
+  lowercaseOpt = prompt("Include lowercase letters in your password? \nYes or No?");
+  
+  if (lowercaseOpt === null){
+    determineLength();
+  } else if (lowercaseOpt === "yes"){
+    lowercaseOpt = true;
+    return lowercaseOpt;
+  } else if (lowercaseOpt === "no"){
+    lowercaseOpt = false;
+    return lowercaseOpt;
+  } else {
+    alert("Must answer Yes or No.");
+    chooseLowercase();
+  }
+  return lowercaseOpt;
+}
+
+// Function used to determine inlcusion of uppercase letters in password
+function chooseUppercase(){
+  uppercaseOpt = prompt("Include uppercase letters in your password? \nYes or No?");
+  
+  if (uppercaseOpt === null){
+    determineLength();
+  } else if (uppercaseOpt === "yes"){
+    uppercaseOpt = true;
+    return uppercaseOpt;
+  } else if (uppercaseOpt === "no"){
+    uppercaseOpt = false;
+    return uppercaseOpt;
+  } else {
+    alert("Must answer Yes or No.");
+    chooseUppercase();
+  }
+  return uppercaseOpt;
+}
+
+// Function used to determine incusion of numbers in password
+function chooseNumbers(){
+  numberOpt = prompt("Include numbers in your password? \nYes or No?");
+
+  if (numberOpt === null){
+    determineLength();
+  } else if (numberOpt === "yes"){
+    numberOpt = true;
+    return numberOpt;
+  } else if (numberOpt === "no"){
+    numberOpt = false;
+    return numberOpt;
+  } else {
+    alert("Must answer Yes or No.");
+    chooseNumbers();
+  }
+  return numberOpt;
+}
+
+// Function used to determine inclusion of special characters in password
+function chooseSpecial(){
+  specialOpt = prompt("Include special characters in your password? \nYes or No?");
+
+  if (specialOpt === null){
+    determineLength();
+  } else if (specialOpt === "yes"){
+    specialOpt = true;
+    return specialOpt;
+  } else if (specialOpt === "no"){
+    specialOpt = false;
+    return specialOpt;
+  } else {
+    alert("Must answer Yes or No.")
+    chooseSpecial();
+  }
+  return specialOpt;
+}
+
+// Function used to call previous prompt functions and generate a password based on the users answers
+function generatePassword(){
+  determineLength();
+  chooseLowercase();
+  chooseUppercase();
+  chooseNumbers();
+  chooseSpecial();
+    
+  var output = "";
+  password = "";
+
+  if (lowercaseOpt && uppercaseOpt && numberOpt && specialOpt){
+    output += lowercase + uppercase + number + special;
+  
+  } else if (lowercaseOpt && uppercaseOpt && numberOpt){
+    output += lowercase + uppercase + number;
+  
+  } else if (lowercaseOpt && specialOpt && numberOpt){
+    output += lowercase + special + number;
+
+  } else if (uppercaseOpt && specialOpt && numberOpt){
+    output += uppercase + special + number;
+
+  } else if (lowercaseOpt && uppercaseOpt){
+    output += lowercase + uppercase;
+
+  } else if (lowercaseOpt && numberOpt){
+    output += lowercase + number;
+
+  } else if (lowercaseOpt && specialOpt){
+    output += lowercase + special;
+
+  } else if (uppercaseOpt && numberOpt){
+    output += uppercase + number;
+
+  } else if (uppercaseOpt && specialOpt){
+    output += uppercase + special;
+
+  } else if (numberOpt && specialOpt){
+    output += number + special;
+
+  } else if (lowercaseOpt){
+    output += lowercase;
+
+  } else if (uppercaseOpt){
+    output += uppercase;
+
+  }else if (numberOpt){
+    output += number;
+
+  } else if (specialOpt){
+    output += special;
+
+  } else {
+    output === null;
+    alert ("Must answer 'Yes' to a minimum of 1 prompt in order to generate password.");
+    determineLength();
+  }
+    // charAt function below used to generate a random password
+    for(var i= 0; i < passwordLength; i++){
+    password += output.charAt(Math.floor(Math.random() * output.length))
+    }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # password-generator
-Randomly generates new passwords
+
+
+## Description 
+
+The main objective of this challenge is to edit the starter code in order to create a functional randomized password generator based off of the criteria provided.
+ 
+\
+User Story
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+\
+Acceptance Criteria
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Process
+
+\
+Edits made to JS file
+```
+1. Created variables for each character type 
+    - (lowercase, uppercase, number, special)
+
+2. Created functions to display prompts for user to respond to in order generate password
+    - determineLength();
+    - chooseLowercase();
+    - chooseUppercase();
+    - chooseNumbers();
+    - chooseSpecial();
+
+3. Created function to generate password using charAt method 
+    - Called previous prompt functions
+    - Created output variable
+    - Utilized if-else statements to determine users input
+```
+
+## Screenshots
+![](assets/images/pass-gen-initial.jpg)
+![](assets/images/pass-gen-after.jpg)
+
+## Links
+<a href="https://devinlachnicht.github.io/password-generator/" target="_blank">Deployed Application</a>
